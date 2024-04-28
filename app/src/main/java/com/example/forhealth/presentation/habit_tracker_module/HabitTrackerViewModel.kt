fdf9c8a5ce2e5fb3559747d105bd5901eb6fffa6
Kardonok <146772225+Kardonok@users.noreply.github.com>
2024-04-28 11:10:57 +0300

    added all the necessary functions without implementation

--- app/src/main/java/com/example/forhealth/presentation/habit_tracker_module/HabitTrackerViewModel.kt
+++ app/src/main/java/com/example/forhealth/presentation/habit_tracker_module/HabitTrackerViewModel.kt
@@ -1,14 +1,49 @@
 package com.example.forhealth.presentation.habit_tracker_module
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.CreationExtras
 import com.example.forhealth.data.repository.App
 import com.example.forhealth.data.repository.Database
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class HabitTrackerViewModel(val database:Database):ViewModel() {
+
     val itemsList = database.getHabitTrackerDao().getAllHabits()
 
+    val habit = mutableStateOf("")
+    val addCardIsOpen = mutableStateOf(false)
+    fun removeFromDatabase()
+    {
+        viewModelScope.launch {  }
+    }
+    fun addToDatabase()
+    {
+        viewModelScope.launch {  }
+    }
+
+    fun updateDatabaseItem()
+    {
+        viewModelScope.launch {  }
+    }
+
+    fun fixCurrentTime()
+    {
+
+    }
+
+    fun timeСalculation()
+    {
+
+    }
+
+    fun timeFormator()
+    {
+
+    }
 
     companion object{
         val factory: ViewModelProvider.Factory = object: ViewModelProvider.Factory{
