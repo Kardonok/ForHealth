6605cea2408fe7924ac881755495cd0bfee6890b
Kardonok <146772225+Kardonok@users.noreply.github.com>
2024-06-01 08:52:45 +0300

    api changed

--- app/src/main/java/com/example/forhealth/network/MainApiModule.kt
+++ app/src/main/java/com/example/forhealth/network/MainApiModule.kt
@@ -1,9 +1,17 @@
 package com.example.forhealth.network
 
+import com.example.forhealth.network.models.ProfileApiItem
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
 
 
 private const val BASE_URL = ""
@@ -15,7 +23,21 @@ private val retrofit = Retrofit.Builder()
 
 interface MainApiService
 {
+    //Регистрация - отправляем данные на проверку, получаем либо ошибку, либо успех с готовым профилем со ВСЕМИ полями
+    @POST("registration")
+    suspend fun registerUserOnServer(@Body user:ProfileApiItem): Response<ProfileApiItem>
 
+    //Вход - отправляем логин и пароль на проверку, при нахождении профиля в бд получаем профиль со ВСЕМИ полями
+    @GET("login")
+    suspend fun getUserFromServer(@Query("user_name") userName:String, @Query("user_password") userPassword:String): Response<ProfileApiItem>
+
+    //Изменение - отправляем профиль со ВСЕМИ данными, при нахождении профиля по id и токену получаем измененый профиль
+    @PUT("change")
+    suspend fun changeUserOnServer(@Body user: ProfileApiItem): Response<ProfileApiItem>
+
+    //Удаление - отправляем локальный id и токен, при нахождении на сервере переданного id и токена профиль удаляется
+    @DELETE("delete")
+    suspend fun deleteUserFromServer(@Query("user_id") userId:Int, @Query("user_token") userToken:String): Response<Void>
 }
 
 object MainApiModule
